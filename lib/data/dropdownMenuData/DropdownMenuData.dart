e4483ac9d35e21c44a8db35d01ab9ad0af25caa2
Berk Erdoğan <erdgnberk@gmail.com>
2024-07-01 19:26:27 +0300

    Firebase integration
    Firebase integration was done

---
+++ lib/data/dropdownMenuData/DropdownMenuData.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+enum ProfileLable{
+  bear('bear', "assets/animalIcon/bear.svg"),
+  cat('cat', "assets/animalIcon/cat.svg"),
+  chick('chick', "assets/animalIcon/chick.svg"),
+  cow('cow', "assets/animalIcon/cow.svg"),
+  deer('deer', "assets/animalIcon/deer.svg"),
+  dog('dog', "assets/animalIcon/dog.svg"),
+  octopus('octopus', "assets/animalIcon/octopus.svg"),
+  rabbit('rabbit', "assets/animalIcon/rabbit.svg"),
+  seal('seal', "assets/animalIcon/seal.svg"),
+  sheep('sheep', "assets/animalIcon/sheep.svg"),
+  tiger('tiger', "assets/animalIcon/tiger.svg"),
+  whale('whale', "assets/animalIcon/whale.svg");
+
+
+  const ProfileLable(this.label, this.icon);
+  final String label;
+  final dynamic  icon;
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:motix_app/login_page.dart';
-import 'package:motix_app/onboarding_screen.dart';
-import 'package:motix_app/register_page.dart';
-import 'package:motix_app/splash_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:motix_app/pages/cubit/registerCubit.dart';
+import 'package:motix_app/pages/splash_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -14,14 +16,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => RegisterCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home:  SplashScreen(),
       ),
-      home:  SplashScreen(),
     );
   }
 }
