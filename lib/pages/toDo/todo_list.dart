e7fb08cf4ccd98eb88af1a6fb43bd1b2ff9e7cf3
azizexb <azizedirilik6@gmail.com>
2024-07-29 22:17:52 +0300

    all bugs fixed for todo page

--- lib/pages/toDo/todo_list.dart
+++ lib/pages/toDo/todo_list.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:motix_app/pages/toDo/edit_todo_page.dart';
+import 'package:intl/intl.dart';
 
-class TodoItem extends StatelessWidget {
+class TodoItem extends StatefulWidget {
   const TodoItem({
     Key? key,
     required this.taskName,
@@ -10,6 +10,9 @@ class TodoItem extends StatelessWidget {
     required this.onChanged,
     required this.deleteFunction,
     required this.editFunction,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
   }) : super(key: key);
 
   final String taskName;
@@ -17,6 +20,29 @@ class TodoItem extends StatelessWidget {
   final ValueChanged<bool?>? onChanged;
   final Function(BuildContext)? deleteFunction;
   final Function(BuildContext, String)? editFunction;
+  final DateTime date;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+
+  @override
+  _TodoItemState createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  late bool _taskCompleted;
+
+  @override
+  void initState() {
+    super.initState();
+    _taskCompleted = widget.taskCompleted;
+  }
+
+  void _handleCheckboxChange(bool? value) {
+    setState(() {
+      _taskCompleted = value ?? false;
+    });
+    widget.onChanged?.call(value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,18 +53,52 @@ class TodoItem extends StatelessWidget {
           motion: const StretchMotion(),
           children: [
             SlidableAction(
-              onPressed: deleteFunction,
+              onPressed: widget.deleteFunction,
               icon: Icons.delete,
               backgroundColor: Colors.red,
               borderRadius: BorderRadius.circular(15),
             ),
           ],
         ),
-        child: TodoItemContainer(
-          taskCompleted: taskCompleted,
-          onChanged: onChanged,
-          taskName: taskName,
-          editFunction: editFunction,
+        child: GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                backgroundColor: Color(0xFFCDE8E5),
+                title: const Text(
+                  'Task Details',
+                  style: TextStyle(color: Colors.black),
+                ),
+                content: TodoItemDialog(
+                  taskCompleted: _taskCompleted,
+                  onChanged: _handleCheckboxChange,
+                  taskName: widget.taskName,
+                  date: widget.date,
+                  startTime: widget.startTime,
+                  endTime: widget.endTime,
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text(
+                      'Kapat',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          child: TodoItemContainer(
+            taskCompleted: _taskCompleted,
+            onChanged: _handleCheckboxChange,
+            taskName: widget.taskName,
+            editFunction: widget.editFunction,
+            date: widget.date,
+            startTime: widget.startTime,
+            endTime: widget.endTime,
+          ),
         ),
       ),
     );
@@ -52,36 +112,44 @@ class TodoItemContainer extends StatelessWidget {
     required this.onChanged,
     required this.taskName,
     required this.editFunction,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
   }) : super(key: key);
 
   final bool taskCompleted;
   final ValueChanged<bool?>? onChanged;
   final String taskName;
   final Function(BuildContext, String)? editFunction;
+  final DateTime date;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
       padding: TodoItemPadding.containerPadding,
       decoration: BoxDecoration(
-        color: const Color(0xFFfefae0),
-        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
       ),
       child: Row(
         children: [
           Checkbox(
             value: taskCompleted,
             onChanged: onChanged,
-            checkColor: Colors.white,
-            activeColor: Color(0xFFED7D31),
-            side: const BorderSide(color: Colors.black),
           ),
-          const SizedBox(width: 10),
           Expanded(
             child: Text(
-              overflow: TextOverflow.ellipsis,
               taskName,
+              overflow: TextOverflow.ellipsis,
               style: TextStyle(
                 fontSize: 18,
                 color: Colors.black,
@@ -93,19 +161,19 @@ class TodoItemContainer extends StatelessWidget {
               ),
             ),
           ),
-          IconButton(
-            onPressed: () async {
-              final editedTaskName = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditTodoPage(initialTaskName: taskName),
-                ),
-              );
-              if (editedTaskName != null && editFunction != null) {
-                editFunction!(context, editedTaskName);
-              }
-            },
-            icon: Icon(Icons.edit),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                DateFormat('dd-MM-yyyy').format(date),
+                style: const TextStyle(color: Colors.black),
+              ),
+              Text(
+                '${startTime.format(context)} - ${endTime.format(context)}',
+                style: const TextStyle(color: Colors.black),
+              ),
+            ],
           ),
         ],
       ),
@@ -113,8 +181,81 @@ class TodoItemContainer extends StatelessWidget {
   }
 }
 
+class TodoItemDialog extends StatefulWidget {
+  const TodoItemDialog({
+    Key? key,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.taskName,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
+  }) : super(key: key);
+
+  final bool taskCompleted;
+  final ValueChanged<bool?>? onChanged;
+  final String taskName;
+  final DateTime date;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+
+  @override
+  _TodoItemDialogState createState() => _TodoItemDialogState();
+}
+
+class _TodoItemDialogState extends State<TodoItemDialog> {
+  late bool _taskCompleted;
+
+  @override
+  void initState() {
+    super.initState();
+    _taskCompleted = widget.taskCompleted;
+  }
+
+  void _handleCheckboxChange(bool? value) {
+    setState(() {
+      _taskCompleted = value ?? false;
+    });
+    widget.onChanged?.call(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: _taskCompleted,
+              onChanged: _handleCheckboxChange,
+            ),
+            Expanded(
+              child: Text(
+                widget.taskName,
+                style: TextStyle(
+                  decoration:
+                      _taskCompleted ? TextDecoration.lineThrough : null,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Text(
+          DateFormat('dd-MM-yyyy').format(widget.date),
+          style: const TextStyle(color: Colors.black),
+        ),
+        Text(
+          '${widget.startTime.format(context)} - ${widget.endTime.format(context)}',
+          style: const TextStyle(color: Colors.black),
+        ),
+      ],
+    );
+  }
+}
+
 class TodoItemPadding {
-  static const EdgeInsets itemPadding =
-      EdgeInsets.symmetric(vertical: 8, horizontal: 15);
-  static const EdgeInsets containerPadding = EdgeInsets.all(20);
+  static const EdgeInsets itemPadding = EdgeInsets.symmetric(vertical: 5.0);
+  static const EdgeInsets containerPadding = EdgeInsets.all(15.0);
 }
